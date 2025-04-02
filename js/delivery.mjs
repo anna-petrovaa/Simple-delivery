745b9ad3c4afb15404ce218242d92d18a4dc1f01
Anna Petrova <petrovaanna2205@gmail.com>
2025-04-03 01:29:58 +0700

    комит

--- js/delivery.mjs
+++ js/delivery.mjs
@@ -5,6 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const expressPrice = document.querySelector(".express-price");
   const expressDay = document.querySelector(".express-span");
+
+  const radioStandart = document.querySelector(".standart-radio-input");
+  const radioExpress = document.querySelector(".express-radio-input");
+
+  const expressCardContainer = document.querySelector(
+    ".express-card-container"
+  );
+  const standartCardContainer = document.querySelector(
+    ".standart-card-container"
+  );
+
   let calculateData = JSON.parse(localStorage.getItem("calculateData"));
 
   if (!calculateData) {
@@ -16,4 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   expressPrice.textContent = `${calculateData.options[1].price} ₽`;
   expressDay.textContent = `${calculateData.options[1].days} рабочих дней`;
+
+  function choseDelivery() {
+    if (radioStandart.checked) {
+      standartCardContainer.classList.add("checked");
+      expressCardContainer.classList.remove("checked");
+    }
+
+    if (radioExpress.checked) {
+      standartCardContainer.classList.remove("checked");
+      expressCardContainer.classList.add("checked");
+    }
+  }
+
+  radioStandart.addEventListener("change", choseDelivery);
+  radioExpress.addEventListener("change", choseDelivery);
 });
